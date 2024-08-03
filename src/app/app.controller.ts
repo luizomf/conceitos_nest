dfa86aae63d1ba500829967d9b47daac87f0641d
Luiz Otávio <luizomf@gmail.com>
2024-08-03 10:24:42 -0300

    Básico sobre protocolo HTTP, URLs, recursos e métodos de solicitação (GET)

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+// import { AppService } from './app.service';
 
-@Controller()
+@Controller('home') // /home
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
 
-  @Get()
+  // Método da solicitação -> Ler (Read) -> CRUD
+  // /home/hello
+  @Get('hello') 
   getHello(): string {
-    return this.appService.getHello();
+    return 'Qualquer coisa.';
   }
 }
