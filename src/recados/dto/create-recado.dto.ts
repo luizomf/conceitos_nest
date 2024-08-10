34086547a3de268dc309313357cba561575c4dde
Luiz Otávio <luizomf@gmail.com>
2024-08-10 09:25:08 -0300

    Validando dados de entrada com useGlobalPipes, ValidationPipe e class-validator

--- src/recados/dto/create-recado.dto.ts
+++ src/recados/dto/create-recado.dto.ts
@@ -1,5 +1,21 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateRecadoDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
   readonly texto: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   readonly de: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   readonly para: string;
 }
