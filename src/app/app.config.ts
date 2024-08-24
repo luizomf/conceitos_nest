73f70ffc495b3d175ff8ebfc341404d76133b664
Luiz Otávio <luizomf@gmail.com>
2024-08-24 14:11:13 -0300

    Usando o ConfigService para obter valores do .env diretamente no módulo

---
+++ src/app/app.config.ts
@@ -0,0 +1,13 @@
+export default () => ({
+  database: {
+    type: process.env.DATABASE_TYPE as 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
+    database: process.env.DATABASE_DATABASE,
+    password: process.env.DATABASE_PASSWORD,
+    autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES),
+    synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+  },
+  environment: process.env.NODE_ENV || 'development',
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { AppService } from './app.service';
 import { RecadosModule } from 'src/recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
+import appConfig from './app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      // envFilePath: ['env/.env'],
-      // ignoreEnvFile: true,
+      load: [appConfig],
       validationSchema: Joi.object({
         DATABASE_TYPE: Joi.required(),
         DATABASE_HOST: Joi.required(),
@@ -23,15 +23,23 @@ import * as Joi from '@hapi/joi';
         DATABASE_SYNCHRONIZE: Joi.number().min(0).max(1).default(0),
       }),
     }),
-    TypeOrmModule.forRoot({
-      type: process.env.DATABASE_TYPE as 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USERNAME,
-      database: process.env.DATABASE_DATABASE,
-      password: process.env.DATABASE_PASSWORD,
-      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES), // Carrega entidades sem precisar especifica-las
-      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // Sincroniza com o BD. Não deve ser usado em produção
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: configService.get<'postgres'>('database.type'),
+          host: configService.get<string>('database.host'),
+          port: configService.get<number>('database.port'),
+          username: configService.get<string>('database.username'),
+          database: configService.get<string>('database.database'),
+          password: configService.get<string>('database.password'),
+          autoLoadEntities: configService.get<boolean>(
+            'database.autoLoadEntities',
+          ),
+          synchronize: configService.get<boolean>('database.synchronize'),
+        };
+      },
     }),
     RecadosModule,
     PessoasModule,
