f6adfb6c9c0cd9ed7ddc6d1bd097edd66382618d
Luiz Otávio <luizomf@gmail.com>
2024-08-25 07:53:03 -0300

    Autenticando o usuário com email e senha usando o Hash de senha e HashingService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,12 @@ import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
 
 @Global()
 @Module({
+  imports: [TypeOrmModule.forFeature([Pessoa])],
   controllers: [AuthController],
   providers: [
     {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { Repository } from 'typeorm';
+import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HashingService } from './hashing/hashing.service';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(Pessoa)
+    private readonly pessoaRepository: Repository<Pessoa>,
+    private readonly hashingService: HashingService,
+  ) {}
+
   async login(loginDto: LoginDto) {
-    return loginDto;
+    const pessoa = await this.pessoaRepository.findOneBy({
+      email: loginDto.email,
+    });
+
+    if (!pessoa) {
+      throw new UnauthorizedException('Pessoa não existe.');
+    }
+
+    const passwordIsValid = await this.hashingService.compare(
+      loginDto.password,
+      pessoa.passwordHash,
+    );
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Senha inválida!');
+    }
+
+    // A gente vai fazer o novo token e vamos entregar para o usuário na
+    // resposta.
+
+    return {
+      message: 'Usuário logado!',
+    };
   }
 }
