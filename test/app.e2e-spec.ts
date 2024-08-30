3de7443a7e9fae0e75c1943b17de5e8be9a400da
Luiz Otávio <luizomf@gmail.com>
2024-08-30 04:31:34 -0300

    Testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,8 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app/app.module';
 
+// Testing
+
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
