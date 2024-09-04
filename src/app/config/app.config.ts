e0c182f49c3b7541a332174f71cc6c121f4d18ae
Luiz Otávio <luizomf@gmail.com>
2024-09-04 05:38:39 -0300

    Remove console.log

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -2,7 +2,6 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ParseIntIdPipe } from 'src/common/pipes/parse-int-id.pipe';
 
 export default (app: INestApplication) => {
-  console.log('Configurando o app...');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // remove chaves que não estão no DTO
